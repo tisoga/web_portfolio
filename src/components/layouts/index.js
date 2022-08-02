2b4b6db9facf3b047901f968fead01ca2099fb6b
tisoga <ryan.afrizal20@gmail.com>
2022-08-02 18:36:32 +0700

    fix some files

--- src/components/layouts/index.js
+++ src/components/layouts/index.js
@@ -2,12 +2,10 @@ import Sidebar from './Sidebar/'
 import Home from './Home'
 import Skill from './Skill/'
 import Portfolio from './Project/'
-// import Playground from './playground/FlipCard'
 
 export {
     Sidebar,
     Home,
     Skill,
     Portfolio,
-    // Playground
 }
